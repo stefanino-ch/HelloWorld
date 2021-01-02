3a1d1f5201bd289b9afec7d5b3e517620bbd794a
stefanino-ch <mail@sfeuz.ch>
2021-01-02 05:11:00 +0100

    Introduced pyinstaller action.

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -1,3 +1,23 @@
-# src/hello.py
+import sys
 
-print("Hello world!")
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton 
+from .Klasse import MeineKlasse
+
+# Subclass QMainWindow to customize your application's main window
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()  
+        
+        klasse = MeineKlasse()
+        klasse.sayHello()
+        
+        self.setWindowTitle("My App")        
+        button = QPushButton("Press Me!") # Set the central widget of the Window.
+        self.setCentralWidget(button)  
+        
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+        
+app.exec_()
---
+++ src/Klasse.py
@@ -0,0 +1,20 @@
+'''
+Created on 02.01.2021
+
+@author: user
+'''
+
+class MeineKlasse():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        
+    def sayHello(self):
+        print("Hello")
